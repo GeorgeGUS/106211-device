9b29907b9afb3836e3483fabc1a544d4974c2b0e
GeorgeGUS <georgegus1@gmail.com>
2017-08-27 18:37:52 +0300

    Предпроверочный коммит. Поправил все стили и отступы в соответствии с макетом.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,52 @@
 'use strict';
 
 var popupWrapper = document.querySelector('.popup');
-var popup = popupWrapper.querySelectorAll('.popup__item');
+var activePopup;
 var map = popupWrapper.querySelector('.map');
 var feedback = popupWrapper.querySelector('.feedback');
 var mapOpen = document.querySelector('.contacts__map-link');
 var feedbackOpen = document.querySelector('.contacts__btn');
 
-// var openPopup = function (window) {
-//   popupWrapper.classList.remove('visually-hidden');
-//   window.classList.remove('hidden');
-//   window.addEventListener('click', closePopup);
-// };
+var openPopup = function (evt) {
+  evt.preventDefault();
+  var target = evt.currentTarget.classList;
+  if (target.contains('contacts__map-link')) {
+    activePopup = map;
+  } else if (target.contains('contacts__btn')) {
+    activePopup = feedback;
+  }
+  activePopup.classList.remove('hidden');
+  activePopup.addEventListener('click', onCloseBtnClick);
+  popupWrapper.classList.remove('visually-hidden');
+  document.addEventListener('keydown', onEscPress);
+};
 
-var closePopup = function (evt) {
-  var target = evt.currentTarget;
-  var close = evt.target;
+var closePopup = function () {
+  activePopup.removeEventListener('click', onCloseBtnClick);
+  activePopup.classList.add('hidden');
+  activePopup = undefined;
+  popupWrapper.classList.add('visually-hidden');
+  document.removeEventListener('keydown', onEscPress);
+};
 
-  if (close.classList.contains('popup__close-btn')) {
-    target.removeEventListener('click', closePopup);
-    target.classList.add('hidden');
-    popupWrapper.classList.add('visually-hidden');
+var onCloseBtnClick = function (evt) {
+  if (evt.target.classList.contains('popup__close-btn')) {
+    closePopup();
   }
 };
 
-mapOpen.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popupWrapper.classList.remove('visually-hidden');
-  map.classList.remove('hidden');
-  map.addEventListener('click', closePopup);
-});
+var onWrapperClick = function (evt) {
+  if (evt.target === evt.currentTarget) {
+    closePopup();
+  }
+};
 
-feedbackOpen.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popupWrapper.classList.remove('visually-hidden');
-  feedback.classList.remove('hidden');
-  feedback.addEventListener('click', closePopup);
-});
+var onEscPress = function (evt) {
+  if (evt.keyCode === 27) {
+    closePopup();
+  }
+};
 
+mapOpen.addEventListener('click', openPopup);
+feedbackOpen.addEventListener('click', openPopup);
+popupWrapper.addEventListener('click', onWrapperClick);
